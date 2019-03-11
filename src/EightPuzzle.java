955537545e1fee40af161ca110f9c893c8599309
Kevin Ramirez <kevin.ramirez3@upr.edu>
2019-03-11 17:51:12 -0400

    finished iterative implem entation, it works for most of given initial states

--- src/EightPuzzle.java
+++ src/EightPuzzle.java
@@ -1,42 +1,32 @@
 import java.util.ArrayDeque;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 import java.util.Queue;
 
+/** Giving an 8-puzzle with a 3×3 board with 8 tiles and one empty space (0), The objective 
+ *  is to implement the A* algorithm to solve this puzzle.
+ *  - h(n) the heuristic component is the total Manhattan distance of the misplaced tiles.
+ *  - g(n) is the actual cost incurred from the initial state to the current node. 
+ *  - f(n) = g(n) + h(n), is the estimate of the distance from the current node to the goal.
+ */
 public class EightPuzzle 
-{
-	/** first example **/
-//	int[][] initMatrix = {
-//			{1, 8, 2}, 
-//			{0, 4, 3}, 
-//			{7, 6, 5} 
-//	};
-//	int[][] goalMatrix = {
-//			{1, 2, 3}, 
-//			{4, 5, 6}, 
-//			{7, 8, 0}
-//	};
-	/** second example **/
-	int[][] initMatrix = {
-			{1, 2, 3},
-			{0, 4, 6},
-			{7, 5, 8}
-	};
-	int[][] goalMatrix = {
-			{1, 2, 3},
-			{4, 5, 6},
-			{7, 8, 0}
-	};
-
+{	
+	// queue that keeps track of all states (frontier)
 	Queue<State> qTrack = new ArrayDeque<State>();
+	
+	// shall contain the path from initial state to goal state.
 	LinkedList<State> path = new LinkedList<>();
+	
+	// map integer with 2d array position.  [int -> "row,col"]
 	static Map<Integer, String> goalMap;
 	
 	
-	public EightPuzzle() {
+	public EightPuzzle(int[][] initMatrix, int[][] goalMatrix) {
 		goalMap = new HashMap<>();
-		// map integer with 2d array position.  [int -> "row, col"]
+		
 		for (int i = 0; i < goalMatrix.length; i++)
 			for (int k = 0; k < goalMatrix[i].length; k++)
 				goalMap.put(goalMatrix[i][k], i+","+k);
@@ -44,43 +34,60 @@ public class EightPuzzle
 		qTrack.add(new State(initMatrix));
 	}
 	
-	
-	public void solve() {
+	/** Stores the path that it takes to solve the puzzle from the 
+	 *  initial state only goal state on the instance list <i>path</i>. */
+	private void storePath() {
 		State peek = qTrack.peek();
-		if (peek.isSolved()) {
-			while (peek != null) {
-				path.addFirst(peek);
-				peek = peek.getParent();
-			}
-			return;
+		while (peek != null) {
+			path.addFirst(peek);
+			peek = peek.getParent();
 		}
+	}
 
-		peek.evaluateChildStates();
-		
-		for (State puzzleState : peek.childStates) {
-			puzzleState.parent = qTrack.peek();
-			qTrack.add(puzzleState);
-		}
-		
-		qTrack.poll();
-		solve();
-	}
-	
-	public void printInitGoalStates() {
-		System.out.println("INITIAL STATE");
-		printMatrix(initMatrix);
-		
-		System.out.println("GOAL STATE");
-		printMatrix(goalMatrix);
-	}
-	
-	
-	public void printPath() {
-		System.out.println("----- PATH FROM INITIAL TO GOAL STATE -----");
-		for (State state : this.path) {
-			printMatrix(state.matrix);
+	/** Iterative process of solving the puzzle. */
+	public void solve() {
+		while (!qTrack.isEmpty()) {
+			State peek = qTrack.peek();
+			if (peek.isSolved()) {
+				this.storePath();
+				return;
+			}
+			
+			peek.evaluateChildStates();
+			
+			// add the Next States to be evaluated into the queue
+			for (State puzzleState : peek.childStates) {
+				puzzleState.parent = qTrack.peek();
+				qTrack.add(puzzleState);
+			}
+			
+			// dequeue the first state for it to not be evaluated again
+			qTrack.poll();
 		}
 	}
+	
+	/** checks if the given matrix is admissible by checking if the 
+	 * total number of inversion is even.
+	 * @param matrix the 2d dimensional array representation of the puzzle
+	 * @return true if total number of inversion is even.  Otherwise, false.
+	 */
+	public boolean isAdmissible(int[][] matrix) {
+		Integer[] arr = new Integer[matrix.length*matrix[0].length];
+		int count = 0, k = 0;		
+		
+		for (int i = 0; i < matrix.length; i++)
+			for (int j = 0; j < matrix[i].length; j++)
+				arr[k++]= matrix[i][j];
+		
+		// count how many inversions the list has
+		for (int i = 0; i < arr.length-1; i++)
+			for (int j = i+1; j < arr.length; j++)
+				if (arr[i] != 0 && arr[j] != 0 && arr[i] > arr[j])
+					count++;
+		
+		return count % 2 == 0;
+	}
+
 	
 	public void printMatrix(int[][] matrix) {
 		for (int r = 0; r < matrix.length; r++) {
@@ -91,45 +98,51 @@ public class EightPuzzle
 		System.out.println();
 	}
 	
+
+	public void printPath() {
+		System.out.println("----- PATH FROM INITIAL TO GOAL STATE -----");
+		for (State state : this.path)
+			printMatrix(state.matrix);
+	}
+
 	
-	
-	public static void main(String[] args) {
-		EightPuzzle eightPuzzle = new EightPuzzle();
-		eightPuzzle.printInitGoalStates();
-		eightPuzzle.solve();
-		eightPuzzle.printPath();
+	public static void main(String[] args) 
+	{	
+		/** examples that pass **/
+		int[][] initMatrix = { {1, 8, 2}, {0, 4, 3}, {7, 6, 5} };
+		int[][] goalMatrix = { {1, 2, 3}, {4, 5, 6}, {7, 8, 0} };
+		
+//		int[][] initMatrix = { {1, 2, 3}, {0, 4, 6}, {7, 5, 8} };
+//		int[][] goalMatrix = { {1, 2, 3}, {4, 5, 6}, {7, 8, 0} };
+		
+//		int[][] initMatrix = { {1, 3, 6}, {5, 0, 2}, {4, 7, 8} };
+//		int[][] goalMatrix = { {1, 2, 3}, {4, 5, 6}, {7, 8, 0} };
+		
+		
+		/** examples that DON'T PASS **/
+//		int[][] initMatrix = {
+//				{8, 0, 6}, 
+//				{5, 4, 7},
+//				{2, 3, 1}
+//		};
+//		int[][] goalMatrix = {
+//				{0, 1, 2}, 
+//				{3, 4, 5}, 
+//				{6, 7, 8}
+//		};
+		
+		EightPuzzle eightPuzzle = new EightPuzzle(initMatrix, goalMatrix);
+		System.out.println("INITIAL STATE");
+		eightPuzzle.printMatrix(initMatrix);
+		
+		System.out.println("GOAL STATE");
+		eightPuzzle.printMatrix(goalMatrix);
+
+		
+		if (eightPuzzle.isAdmissible(initMatrix)) {
+			eightPuzzle.solve();
+			eightPuzzle.printPath();
+		}
+		else System.out.println("Is not possible to solve the given initial state");
 	}
 }
-
-
-
-
-
-
-
-
-
-//if (currState.isSolved())
-//	return;
-//
-//currState.evaluateChildStates();
-//int fMin = Integer.MAX_VALUE;
-//
-//int i = 0;
-//int[] estimatedDistances = new int[currState.childPuzzleState.size()];
-//for (PuzzleState puzzleState : currState.childPuzzleState) {
-//	int estimatedDistance = puzzleState.estimatedDistance();
-//	estimatedDistances[i++] = estimatedDistance;
-//	if (estimatedDistance < fMin)
-//		fMin = estimatedDistance;
-//}
-//
-//
-//for (i = 0; i < currState.childPuzzleState.size(); i++) {
-//	if (estimatedDistances[i] == fMin) {
-//		currState.childPuzzleState.get(i).costIncurred++;
-//		this.currState = currState.childPuzzleState.get(i);
-//		solve();
-//		return;
-//	}
-//}
--- src/State.java
+++ src/State.java
@@ -1,6 +1,10 @@
 import java.util.LinkedList;
 
-/** */
+/** A State is an object representing a state of the 8-puzzle.  More formally,
+ *  represents one node of the binary tree that all path of properties of a State include the following:
+ *  - The matrix: values from 0 to 8 of the puzzle within a 2d array. 
+ *  - Child States: list of the next states of the binary tree
+ */
 public class State
 {
 	int[][] matrix;
@@ -17,6 +21,9 @@ public class State
 	public State (int[][] matrix, int costIncurred) {
 		this(matrix, null, costIncurred);
 	}
+	/** 
+	 *  
+	 */
 	public State(int[][] matrix, State parent, int costIncurred) { 
 		this.matrix = matrix;
 		this.parent = parent;
@@ -45,17 +52,18 @@ public class State
 			}
 		}
 	}
-
+	
 	public State getParent() { return this.parent; }
 		
-
+	/** true if the instance containing the total Manhattan Distance 
+	 *  equals zero.  Otherwise false. */
 	public boolean isSolved() {
 		return totalManhattanDistance == 0;
 	}
 
 
 	public int estimatedDistance() {
-		return totalMisplaced() + totalManhattanDistance;
+		return costIncurred + totalManhattanDistance;
 	}
 
 
@@ -68,38 +76,45 @@ public class State
 		return count;
 	}
 
-
+	/** Evaluate each child state and only stores the ones
+	 *  that have the least amount of estimated distance f(n). 
+	 */
 	public void evaluateChildStates() {
 		if (!childStates.isEmpty()) return;
 			
 		childStates = new LinkedList<>();
+		// row and column position of the zero within goal state
 		int rZero = Integer.parseInt(zeroPos.substring(0, 1));
 		int cZero = Integer.parseInt(zeroPos.substring(2));
 
-		// store the integer value that are above, right, below, and left of zero
-		// (-1 means that is out of bounds)
+		// store the integer values that are above, right, below, and 
+		// left of the zero value.  -1 represents that the particular 
+		// direction is out of bounds
 		int up = -1, right = -1, down = -1, left = -1;
 		if (validRowOrCol(rZero-1)) up    = matrix[rZero-1][cZero];
 		if (validRowOrCol(cZero-1)) left  = matrix[rZero][cZero-1];
 		if (validRowOrCol(cZero+1)) right = matrix[rZero][cZero+1];
 		if (validRowOrCol(rZero+1)) down  = matrix[rZero+1][cZero];
 
+		// keeps track of previous estimated distances along the process
 		int prevDist = Integer.MAX_VALUE;
+		
+		// evaluate/iterate over all values within the 2d array
 		for (int r = 0; r < matrix.length; r++) {
 			for (int c = 0; c < matrix[r].length; c++) {
-				int curr = matrix[r][c];
-
-				if (curr == up || curr == right || curr == down || curr == left) {
+				int currVal = matrix[r][c];
+				
+				// check if current value is either above, right, below, or left of the zero value
+				if (currVal == up || currVal == right || currVal == down || currVal == left) {
 					int[][] newState = shallowCopyInstanceMatrix();
 					swap(newState, rZero, cZero, r, c);
-//					childPuzzleState.add(new PuzzleState(newState, this.costIncurred+1));
 
-					/*************/
 					State currState = new State(newState, this.costIncurred+1);
 					int currDist = currState.estimatedDistance();
 
+					/*********************/
 					// ensures to only store the child states that 
-					// have the least amount of estimated distance.
+					// have the least estimated distance.
 					if (currDist < prevDist)
 						childStates.removeAll(childStates);
 
@@ -107,13 +122,13 @@ public class State
 						prevDist = currDist;
 						childStates.add(currState);
 					}
-					/*************/
+					/*********************/
 				}
 			}
 		}
 	}
 
-
+	/** */
 	private int[][] shallowCopyInstanceMatrix() {
 		int[][] newState = new int[3][3];
 		for (int r = 0; r < matrix.length; r++)
@@ -122,16 +137,36 @@ public class State
 		return newState;
 	}
 
-
+	/** Swaps the the zero value that is in the <i>(zr, zc)</i> position 
+	 *  within the given matrix with the value in position <i>(r, c)</i>
+	 *  @param matrix the 2d dimensional array representation of the puzzle
+	 *  @param zr the row index of the zero value within the matrix
+	 *  @param zc the column index of the zero value within the matrix
+	 *  @param r  a row index within the matrix
+	 *  @param c  a column index within the matrix
+	 */
 	private void swap(int[][] matrix, int zr, int zc, int r, int c) {
 		int temp = matrix[zr][zc];
 		matrix[zr][zc] = matrix[r][c];
 		matrix[r][c] = temp;
 	}
 
-	/** validates if the given row or column index is within the 3x3 array range */
-	private boolean validRowOrCol(int g) {
-		return g >= 0 && g < 3;
+
+	/** Validates if the given row or column index is within the 3x3 array range 
+	 *  @param i row or column index within the matrix
+	 *  @return true if index is between [0, 3) */
+	private boolean validRowOrCol(int i) {
+		return i >= 0 && i < 3;
+	}
+	
+	/** Print the given matrix and the given distance  */
+	public void printMatrix(int[][] matrix, int dist) {
+		for (int r = 0; r < matrix.length; r++) {
+			for (int c = 0; c < matrix[r].length; c++)
+				System.out.print(matrix[r][c] +" ");
+			System.out.println();
+		}
+		System.out.println(dist);
 	}
 }
 
